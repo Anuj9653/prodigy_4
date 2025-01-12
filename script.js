d216efd6271ee074691176fbeb4035582fa65911
Anuj Gupts <anujgupta9653@gmail.com>
2025-01-12 13:36:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Smooth Scroll for Navigation Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        });
+    });
